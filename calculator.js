ad4917e15a6d3e66ca0ac244cdd1c1d454ab0868
sadamqadeer17 <sadamqadeer27@gmail.com>
2024-05-28 16:53:52 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    { message: "Please enter 1st number", type: "number", name: "FirstNumber" },
+    { message: "Please enter 2nd number", type: "number", name: "SecondNumber" },
+    {
+        message: "Please select one option for operation",
+        type: "list",
+        name: "options",
+        choices: ["add", "subtract", "multiply", "divide"],
+    },
+]);
+if (answers.options == "add") {
+    console.log(answers.FirstNumber + answers.SecondNumber);
+}
+else if (answers.options == "subtract") {
+    console.log(answers.FirstNumber - answers.SecondNumber);
+}
+else if (answers.options == "multiply") {
+    console.log(answers.FirstNumber * answers.SecondNumber);
+}
+else if (answers.options == "divide") {
+    console.log(answers.FirstNumber / answers.SecondNumber);
+}
+else {
+    console.log("please enter numbers again");
+}
